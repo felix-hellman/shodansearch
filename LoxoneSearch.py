24f47c682b2e73959fbb3f9f1882f0c61415c111
Felix Hellman <felixhellman.ro@gmail.com>
2018-03-19 11:55:46 +0100

    Finding server version

--- LoxoneSearch.py
+++ LoxoneSearch.py
@@ -4,12 +4,13 @@ import shodan
 api = shodan.Shodan(Auth.KEY)
 
 try:
-    results = api.search('Server: Loxone')
-
+    results = api.search('Server: Loxone',page=2)
     print('Results found %s' % results['total'])
     for result in results['matches']:
         print('IP: %s' % result['ip_str'])
-        print (result['data'])
+        startIndex = result['data'].find('Server:',0,len(result['data']))
+        endIndex = result['data'].find("\n",startIndex,len(result['data']))
+        print(result['data'][startIndex:endIndex:])
         print ('')
 except (shodan.APIError, e):
     print ('Error: %s' % e)
