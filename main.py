f2c922804793a79248275a9ff15239ae441fdbca
Felix Hellman <felixhellman.ro@gmail.com>
2018-03-22 15:41:00 +0100

    Refactor, Searches are now done withn a consice function

--- main.py
+++ main.py
@@ -9,27 +9,14 @@ resultFolder = "results"
 if not os.path.exists(resultFolder):
     os.makedirs(resultFolder)
 
-LoxoneList = LoxoneSearch.Search()
-PowerLogicList = PowerLogicSearch.Search()
-CarGpsList = CarGpsSearch.Search()
-AGFEOList = AGFEOSearch.Search()
+def PerformSearch(Search):
+    SearchList = Search.Search()
+    with open(resultFolder+"/"+Search.FileName,"a") as output:
+        output.write(Search.Header)
+        for result in SearchList:
+            output.write(result.printFormat())
 
-with open(resultFolder+"/"+LoxoneSearch.FileName,"a") as output:
-    output.write(LoxoneSearch.Header)
-    for result in LoxoneList:
-        output.write(result.printFormat())
-
-with open(resultFolder+"/"+PowerLogicSearch.FileName,"a") as output:
-    output.write(PowerLogicSearch.Header)
-    for result in PowerLogicList:
-        output.write(result.printFormat())
-
-with open(resultFolder+"/"+CarGpsSearch.FileName,"a") as output:
-    output.write(CarGpsSearch.Header)
-    for result in CarGpsList:
-        output.write(result.printFormat())
-
-with open(resultFolder+"/"+AGFEOSearch.FileName,"a") as output:
-    output.write(AGFEOSearch.Header)
-    for result in AGFEOList:
-        output.write(result.printFormat())
+PerformSearch(LoxoneSearch)
+PerformSearch(PowerLogicSearch)
+PerformSearch(CarGpsSearch)
+PerformSearch(AGFEOSearch)
